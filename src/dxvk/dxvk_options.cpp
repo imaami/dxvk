6790f76985d542a096b8972cf1dc2eb110b6a730
Sporif <razorizor@googlemail.com>
2021-01-08 09:29:25 +0000

    Update patch for dxvk commit 2405e474
    Fixes #6
    
    Also extends compatibility to dxvk 1.4.3
    
    Source-commit: 88e6392346dc1677d4898e6d4aa27a9026778e08
    Signed-off-by: Juuso Alasuutari <juuso.alasuutari@gmail.com>

--- src/dxvk/dxvk_options.cpp
+++ src/dxvk/dxvk_options.cpp
@@ -3,16 +3,16 @@
 namespace dxvk {
 
   DxvkOptions::DxvkOptions(const Config& config) {
-    enableAsync           = config.getOption<bool>    ("dxvk.enableAsync",            false);
     enableStateCache      = config.getOption<bool>    ("dxvk.enableStateCache",       true);
     enableOpenVR          = config.getOption<bool>    ("dxvk.enableOpenVR",           true);
     enableOpenXR          = config.getOption<bool>    ("dxvk.enableOpenXR",           true);
-    numAsyncThreads       = config.getOption<int32_t> ("dxvk.numAsyncThreads",        0);
     numCompilerThreads    = config.getOption<int32_t> ("dxvk.numCompilerThreads",     0);
     useRawSsbo            = config.getOption<Tristate>("dxvk.useRawSsbo",             Tristate::Auto);
     useEarlyDiscard       = config.getOption<Tristate>("dxvk.useEarlyDiscard",        Tristate::Auto);
     halveNvidiaHVVHeap    = config.getOption<Tristate>("dxvk.halveNvidiaHVVHeap",     Tristate::Auto);
     hud                   = config.getOption<std::string>("dxvk.hud", "");
+    enableAsync           = config.getOption<bool>    ("dxvk.enableAsync",            false);
+    numAsyncThreads       = config.getOption<int32_t> ("dxvk.numAsyncThreads",        0);
   }
 
 }
